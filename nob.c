4d5ff7ee2d40c87ac20a588b1dee780ac05c94d6
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2024-11-10 09:04:07 +0530

    gray gradient

--- nob.c
+++ nob.c
@@ -6,7 +6,13 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
-    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", "src/randomart", "src/randomart.c");
-    if (!cmd_run_sync(cmd)) return 1;
+    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", "src/randomart", "src/randomart.c", "-lm");
+    if (!cmd_run_sync_and_reset(&cmd)) return 1;
+
+    cmd_append(&cmd, "rm", "-f", "nob.old");
+    if (!cmd_run_sync_and_reset(&cmd)) return 1;
+
+    cmd_append(&cmd, "./src/randomart");
+    if (!cmd_run_sync_and_reset(&cmd)) return 1;
     return 0;
 }
--- src/randomart.c
+++ src/randomart.c
@@ -1,6 +1,57 @@
-#include <stdio.h>
+#include <stdint.h>
+#define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+#include "nob.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define WIDTH 400
+#define HEIGHT WIDTH
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} RGBA32;
+
+static RGBA32 pixels[WIDTH*HEIGHT];
+
+typedef struct {
+    float x, y;
+} Vec2;
+
+typedef struct {
+    float r, g, b;
+} Color;
+
+Color gray_gradient(float x, float y) {
+    UNUSED(y);
+    return (Color) {x, x, x};
+}
+
+void render_pixels(Color (*f)(float x, float y)) {
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        float ny = (float)y / HEIGHT * 2.0f - 1;
+        for (size_t x = 0; x < WIDTH; ++x) {
+            float nx = (float)x / WIDTH * 2.0f - 1;
+            Color c = f(nx, ny);
+            size_t index = y * WIDTH + x;
+            pixels[index].r = (c.r + 1) / 2 * 255;
+            pixels[index].g = (c.g + 1) / 2 * 255;
+            pixels[index].b = (c.b + 1) / 2 * 255;
+            pixels[index].a = 255;
+
+        }
+    }
+}
 
 int main(void) {
-    printf("hi!\n");
+    render_pixels(gray_gradient);
+    const char *output_path = "output.png";
+    if (!stbi_write_png(output_path, WIDTH, HEIGHT, 4, pixels, WIDTH * sizeof(RGBA32))) {
+        nob_log(ERROR, "could not save image: %s", output_path);
+        return 1;
+    };
     return 0;
 }
