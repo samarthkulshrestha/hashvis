6d0955a6f4d8fc644d44a56df524eefc64ee794b
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2024-11-11 14:50:09 +0530

    adding switch-case warning flag

--- nob.c
+++ nob.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
-    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", "src/randomart", "src/randomart.c", "-lm");
+    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum",  "-ggdb", "-o", "src/randomart", "src/randomart.c", "-lm");
     if (!cmd_run_sync_and_reset(&cmd)) return 1;
 
     cmd_append(&cmd, "rm", "-f", "nob.old");
